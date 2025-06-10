bad90633c558e3878c39bd20f8f3d51dbc9320ca
surfshtt <fk8ghtt@gmail.com>
2025-06-10 14:24:52 +0300

    1.0-1.1
    intial+decrypted text animation

---
+++ project/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import DecryptedText from "./DecryptedText.jsx";
+function App() {
+
+
+  return (
+      <div>
+      <DecryptedText text="Hover me!Hover me!Hover me!Hover me!Hover me!Hover me!Hover me!Hover me!Hover me" />
+      <DecryptedText text="Hover me!Hover"/>
+      <DecryptedText text="Hover me!Hover me!Hover me!Hover me!Hover me!Hover me!Hover me!Hover me!Hover me" />
+      </div>
+  )
+}
+
+export default App
